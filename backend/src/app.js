806ffbd88896ad5f1f86835f346438f01903d1b3
denizsetyawan <denizsetyawan95@gmail.com>
2025-12-08 18:02:53 +0700

    add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const productRoutes = require("./routes/productRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 require("dotenv").config();
 
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
